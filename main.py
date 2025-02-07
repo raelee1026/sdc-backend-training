5b5379edd5bf7982f197d778dd205c176a032799
raelee1026 <yylee1026@gmail.com>
2025-02-07 18:59:01 +0800

    hw2

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+from typing import Union
+from enum import Enum
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+
 
 app = FastAPI()
 
@@ -6,3 +16,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
