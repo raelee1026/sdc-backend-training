21352770e49def5ebf8417344d4d02e4f26b7578
raelee1026 <yylee1026@gmail.com>
2025-02-07 23:06:31 +0800

    hw2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
+@app.put("/items/{item_id}")
 async def get_item(item_id: int, item: Item, q: str | None = None):
     result = {"item_id": item_id, **item.model_dump()}
     if q:
