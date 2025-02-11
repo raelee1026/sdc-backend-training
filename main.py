b9bd8d0d99ace14d832c90f486d56af8b90b234e
raelee1026 <yylee1026@gmail.com>
2025-02-11 22:55:36 +0800

    hw3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query, HTTPException
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 from typing import Union, Annotated
 from enum import Enum
@@ -9,7 +9,6 @@ class Item(BaseModel):
     price: float
     tax: float
 
-
 app = FastAPI()
 
 
@@ -18,21 +17,31 @@ async def root():
     return {"message": "Hello World"}
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: Annotated[int,Path(gt=0,le=1000)], 
-                   item: Item, 
-                   q: Annotated[str | None, Query(min_length=3, max_length = 50)] = None):
-    result = {"item_id": item_id, **item.model_dump()}
+async def put_item(item_id: Annotated[int, Path(ge=1, le=1000)], 
+                    q: Annotated[str|None, Query(max_length=50, min_length=3)] = None,
+                    item: Item = None):
+    res = {"item_id": item_id, **item.model_dump()}
     if q:
-        result.update({"q": q})
+        return {**res, "q": q}
+    else :
+        return res
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: Annotated[int,Path(gt=0, le=1000)], 
-                        q: Annotated[str | None, Query(min_length=3, max_length = 50)] = None,
-                        sort_order: str = Query(default="asc", pattern="^(asc|desc)$")):
-    result = {"item_id": item_id}
+async def get_item(
+    item_id: Annotated[int, Path(ge=1, le=1000)], 
+    q: Annotated[str|None, Query(max_length=50, min_length=3)] = None, 
+    sort_order: str = Query(default="asc", pattern="^(asc|desc)$")
+):
     if q:
-        result.update({"description": "This is a sample item that matches the query test_query"})
+        return {
+            "item_id": item_id, 
+            "description": "This is a sample item that matches the query test_query",
+            "sort_order": sort_order
+        }
     else:
-        result.update({"description": "This is a sample item."})
-    result.update({"sort_order":sort_order})
-    return result
+        return {
+            "item_id": item_id,
+            "description": "This is a sample item.",
+            "sort_order": sort_order
+        }
+    return {"item_id": item_id, "q": q}
